d65765ed0a38632dc923e6be6a6dbbc5e5b46736
malekaridhi <malek.aridhii@gmail.com>
2021-01-07 21:26:31 +0100

    score

--- client/src/components/MemorieGame 2/Board.jsx
+++ client/src/components/MemorieGame 2/Board.jsx
@@ -9,18 +9,26 @@ const Board = props => {
   const [cards/*state*/, setCards/*setState*/] = useState(props.cards/*initial state */)
   const [checkers/*store 2 open card  */, setCheckers] = useState([])
   const [completed/*store the type of cards that match in checkers array */, setCompleted] = useState([])
-  const onCardClick = card => () => {
+  const [completedCount, setcompletedCount] = useState(0)
+  const [result, setresult] = useState("")
+  
+ const onCardClick = card => () => {
     //chekersfull compasation to check if we have two cards inside our array checkers
     //cardAlreadyInCheckers check if a card is in checkers
     if (checkersFull(checkers) || cardAlreadyInCheckers(checkers, card)) return
     //the state checkers dose not change immidiatly it needs to be saved in a new variable 
     //newChekers
+    /*setchekersCount(chekersCount-5)*/
+    setcompletedCount(completedCount-5)
     const newCheckers = [...checkers, card]
     setCheckers(newCheckers)
     //validateCheckers confirm that we have the same card 
     const cardsInCheckersMatched = validateCheckers(newCheckers)
     if (cardsInCheckersMatched) {
+      setcompletedCount(completedCount+10)
       setCompleted([...completed, newCheckers[0].type])
+      console.log(completed)
+      
     }
 
     if (checkersFull(newCheckers)) {
@@ -36,13 +44,27 @@ const Board = props => {
     function checkersFull(checkers){
       return checkers.length === 2
     }
+    function  completedFull(completed){
+      return completed.length ===6
+    }
     //wait some time so that the user can see the cards 
     function resetCheckersAfter(time) {
       setTimeout(() => {
         setCheckers([])
       }, time)
     }
+    function score(){
+      if(completedCount===-100){
+        setresult(result+'you lost!')
+      
+      }else if (completedFull(completed)){
+        setresult(result+"Awesome!, You won the game") 
+      
+      }
+    }
+    score()
   }
+  
 //make the cards changed when checkers or completed changed
   useEffect(() => {
     const newCards = cards.map(card => ({
@@ -53,12 +75,24 @@ const Board = props => {
     }))
     setCards(newCards)
   }, [checkers, completed])
-
+  function restart(){
+    setCards(props.cards)
+    setCheckers([])
+    setCompleted([])
+    setcompletedCount(0)
+    setresult("")
+  }
   return (
+    <div>
+        <h2 className='completed'> score : {completedCount}</h2>
+       
     <div className="Board">
       {cards.map(card => (
         <Card {...card} onClick={onCardClick(card)} key={card.id} />
       ))}
+    </div>
+    <button className='result' onClick={restart}>Play again</button>
+    <h2 className='moves'>{result}</h2>
     </div>
   )
 }
